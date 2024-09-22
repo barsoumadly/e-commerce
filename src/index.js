6e9b1e47e73f8b4bb92bf871936771704507ef7c
Barsoum Adly <barsoumadly@gmail.com>
2024-09-22 16:56:07 +0300

    server setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import envVariables from './config/envVariables.js';
 import connectDatabase from './config/database/connectDatabase.js';
@@ -8,6 +9,7 @@ const app = express();
 const port = envVariables.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRoutes);
 
